1381bc97c3e899e8e9177043f968b5c12768cfce
Huzefa Khan <huzefakhan284@gmail.com>
2023-04-16 18:21:55 +0530

    with build

---
+++ Frontend/build/precache-manifest.7fe30a60a3559ca537e05a4b369a3442.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "51ab3cace6c7f32d696a",
+    "url": "/static/css/main.5bdd9098.chunk.css"
+  },
+  {
+    "revision": "51ab3cace6c7f32d696a",
+    "url": "/static/js/main.737a93ea.chunk.js"
+  },
+  {
+    "revision": "935a01a6140831dc693a",
+    "url": "/static/js/runtime~main.e1156fe4.js"
+  },
+  {
+    "revision": "a18023b50a25182743c8",
+    "url": "/static/css/2.dedf6203.chunk.css"
+  },
+  {
+    "revision": "a18023b50a25182743c8",
+    "url": "/static/js/2.5a1bdd66.chunk.js"
+  },
+  {
+    "revision": "0c2ca3d9e678458fef03",
+    "url": "/static/js/3.6a697f1d.chunk.js"
+  },
+  {
+    "revision": "efbe7d116633b3f4ef308e329720d592",
+    "url": "/static/media/main-logo.efbe7d11.png"
+  },
+  {
+    "revision": "508bbc74c176d82afa5037c2825c63b0",
+    "url": "/static/media/user.508bbc74.png"
+  },
+  {
+    "revision": "a426ec56d9ebe9ea496d06518bb1c40d",
+    "url": "/static/media/bg.a426ec56.jpg"
+  },
+  {
+    "revision": "d7f06908eac02994f49c889afcd0b654",
+    "url": "/static/media/logos.d7f06908.png"
+  },
+  {
+    "revision": "3872b434ffb3a09edbbaf9c6ee6b5aa1",
+    "url": "/static/media/Resetpwd.3872b434.svg"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "25ccc7323307b1547523a6017cd65b52",
+    "url": "/index.html"
+  }
+];
